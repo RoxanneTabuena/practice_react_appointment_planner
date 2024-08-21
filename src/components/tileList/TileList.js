7befb220d31530d095e1f677ef5eeb48b9873ec3
Roxanne Tabuena <roxyannet@gmail.com>
2024-08-21 13:18:23 -0700

    "setup TileList.js"

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-export const TileList = () => {
+export const TileList = ({contacts}) => {
+  const list = contacts.map((contact, index) =>{
+    const { name } = contact
+    const description = contact
+    return <Tile name={name} description={description} key={index} />
+  })
   return (
     <div>
-      
+      {list}
     </div>
   );
 };
