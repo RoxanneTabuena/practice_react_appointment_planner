ba3816aa8635d76406a4c1915701997de8e3cb2f
Roxanne Tabuena <roxyannet@gmail.com>
2024-08-21 11:41:16 -0700

    "set up app.js"

--- src/App.js
+++ src/App.js
@@ -1,31 +1,60 @@
-import React from "react";
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom"
+import React, { useState } from "react";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Root, { ROUTES } from "./components/root/Root";
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
 
-  /*
-  Implement functions to add data to
-  contacts and appointments
-  */
+  const addContact = (name, phoneNumber, email) => {
+    const newContact = {
+      name: name,
+      number: phoneNumber,
+      email: email,
+    };
+    setContacts((contacts) => [...contacts, newContact]);
+  };
+  const addAppointment = (name, contact, date, time) => {
+    const newAppt = {
+      name: name,
+      contact: contact,
+      date: date,
+      time: time,
+    };
+    setAppointments((appointments) => [...appointments, newAppt]);
+  };
 
-  const router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/" element={ <Root/> }>
-      <Route index element={ <Navigate to={ROUTES.CONTACTS} replace/> }/>
-      <Route path={ROUTES.CONTACTS} element={ <ContactsPage /> /* Add props to ContactsPage */ }/>
-      <Route path={ROUTES.APPOINTMENTS} element={ <AppointmentsPage /> /* Add props to AppointmentsPage */ }/>
-    </Route>
-  ));
-  
-  return (
-    <RouterProvider router={router}/>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Navigate to={ROUTES.CONTACTS} replace />} />
+        <Route
+          path={ROUTES.CONTACTS}
+          element={
+          <ContactsPage 
+            contacts={contacts} 
+            addContact={addContact} />}
+        />
+        <Route
+          path={ROUTES.APPOINTMENTS}
+          element={
+            <AppointmentsPage
+              appointments={appointments}
+              addAppointment={addAppointment} />}
+        />
+      </Route>
+    )
   );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
