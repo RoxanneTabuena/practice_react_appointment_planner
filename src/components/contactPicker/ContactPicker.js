49e14aae66ff3f4f20f55894c6fcae7ac3ae6505
Roxanne Tabuena <roxyannet@gmail.com>
2024-08-22 06:46:25 -0700

    "setup contactpicker.js"

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({setContact, contacts, contact}) => {
+  const handleChange = ({ target }) => {
+    const contact = contacts.find(contact => contact.name === target.value)
+    setContact(contact)
+  };
   return (
-    <></>
+    <select onChange={handleChange}>
+      <option value="">Please Select</option>
+      {
+       contacts.map( (contact,index) => {
+           return <option value={contact.name} key={index}>{contact.name}</option>
+        })
+       }
+    </select>
   );
 };
