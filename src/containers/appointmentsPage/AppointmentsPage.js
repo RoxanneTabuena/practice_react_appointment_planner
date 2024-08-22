8ddac3e1ed7a94f0fa584a9a2f84098fd9c3d15c
Roxanne Tabuena <roxyannet@gmail.com>
2024-08-22 06:47:04 -0700

    "switch name to purpose"

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -3,16 +3,15 @@ import React, { useState } from "react";
 import { AppointmentForm } from "../../components/appointmentForm/AppointmentForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const AppointmentsPage = ({appointments, newAppointment}) => {
-  const [name, setName] = useState('')
+export const AppointmentsPage = ({appointments, contacts, addAppointment}) => {
+  const [purpose, setPurpose] = useState('')
   const [contact, setContact] = useState({})
   const [date, setDate] = useState('')
   const [time, setTime] = useState('')
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    newAppointment(name, contact, date, time)
-    setName('')
+    addAppointment(purpose, contact, date, time)
+    setPurpose('')
     setContact({})
     setDate('')
     setTime('')
@@ -23,8 +22,8 @@ export const AppointmentsPage = ({appointments, newAppointment}) => {
       <section>
         <h2>Add Appointment</h2>
         <AppointmentForm 
-          name={name}
-          setName={setName}
+          purpose={purpose}
+          setPurpose={setPurpose}
           contact={contact}
           setContact={setContact}
           date={date}
@@ -32,6 +31,7 @@ export const AppointmentsPage = ({appointments, newAppointment}) => {
           time={time}
           setTime={setTime}
           handleSubmit={handleSubmit}
+          contacts={contacts}
         />
       </section>
       <hr />
